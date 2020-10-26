20cee61c2776ed0b222a28201b94d44ed3f3283a
Mohamed Elkhaloui <elkhaloui@gmail.com>
2020-10-26 00:09:11 +0000

    Done.

---
+++ hello_world.rb
@@ -0,0 +1,12 @@
+puts  "Hello World"
+ruby hello_world.ruby
+puts "ESHTA"
+puts "Hello World!"
+puts "Hello, Mohamed
+hello_world.ruby
+puts "hello world"
+
+puts "hello_world!" ]\
+ruby hello_world.rb
+learn test
+
